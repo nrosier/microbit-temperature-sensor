971491160b92dd8a8357a7fd99358e48205805c9
Nick Rosier <github@bunbun.be>
2021-01-01 15:53:43 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+let voltage = 0
+let temperatuur = 0
+basic.forever(function () {
+    voltage = pins.map(
+    0,
+    0,
+    1023,
+    0,
+    3300
+    )
+    temperatuur = voltage - 500 / 10
+    basic.showNumber(temperatuur)
+})
